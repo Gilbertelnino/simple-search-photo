7377120ed17bf8091cf3ef4a967f20edfbaee133
Gilbertelnino <gilbeltelnino@gmail.com>
2021-07-01 20:36:49 +0200

    complete search photo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState();
+  // const [loading, setLoading] = useState(false);
+
+  const inputChange = (e) => {
+    e.preventDefault()
+    setSearch(e.target.value)
+  }
+
+  const fetchPhotos = async () => {
+    if (!search) {
+      setError('Input Can not be empty')
+      return setTimeout(() => setError(''), 5000)
+    }
+    
+    const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${search}/photos`);
+    const data = await response.json();
+    
+    if (data.length === 0) {
+      setError('Album with the given Id not found, Try Again!')
+      
+      return setTimeout(() => setError(''), 5000)
+    }
+    
+    return setPhotos(data);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Search Photo app</h1>
+      <div className="form-search">
+            <input type="search" name="search" placeholder="search photoes by id" value={search} onChange={inputChange} />
+            <button onClick={fetchPhotos}>Get Album Photos By Id</button> 
+      </div>
+      <div style={{color: 'red', fontSize: '1.5rem', textAlign: "center"}}>{error && error }</div>
+      <div className='images-container'>
+        
+        {photos?.map(photo => (
+          <div className="content" key={photo.id}>
+            <img src={photo.thumbnailUrl} alt={ photo.title} />
+            <p>{ photo.title}</p>
+        </div>
+        ))}
+        
+      </div>
     </div>
+
   );
 }
 
